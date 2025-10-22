3cef08fe1236b5baab79f33a42fc5f7804382f30
devil <europedreadlydevil@gmail.com>
2025-10-22 22:08:03 +0300

    Fix: Remove explicit system declaration
    The system was being hardcoded, causing issues on other architectures. Removing this ensures that the system is correctly inherited.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: 
       let 
-        system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };
 
         rustSrc = pkgs.lib.cleanSourceWith {
