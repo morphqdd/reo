397ee93ea254c2b0e88fd7761124831b3d729e2a
devil <europedreadlydevil@gmail.com>
2025-10-22 22:01:55 +0300

    Initial commit: Add flake.nix and flake.lock
    These files enable reproducible builds using Nix. The flake.nix
    defines the build environment and dependencies, while flake.lock
    pinpoints the exact versions for reproducibility.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+
+        rustSrc = pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter = name: type: !builtins.elem ( baseNameOf name ) [ "./target" ".git" ];
+        };
+
+        manifestPath = "${toString rustSrc}/Cargo.toml";
+        manifest = builtins.fromTOML ( builtins.readFile manifestPath );
+
+      in {
+        packages."${system}".default = pkgs.rustPlatform.buildRustPackage rec {
+          pname = manifest.package.name;
+          version = manifest.package.version;
+          src = rustSrc;
+          cargoLock.lockFile = "${src}/Cargo.lock";
+        };
+      }
+    );
+}
